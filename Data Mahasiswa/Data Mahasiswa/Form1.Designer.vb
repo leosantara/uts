766afbb11244de16942f43a5995c1a603032131a
leosantara <93345048+leosantara@users.noreply.github.com>
2023-03-29 07:35:36 +0700

    Add files via upload

---
+++ Data Mahasiswa/Data Mahasiswa/Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnLoadData = New System.Windows.Forms.Button()
+        Me.gbDataMahasiswa = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.tbIPKinGB = New System.Windows.Forms.TextBox()
+        Me.tbNamaInGB = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tbNimInGB = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btnFirst = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbNIM = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.gbDataMahasiswa.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnLoadData
+        '
+        Me.btnLoadData.Location = New System.Drawing.Point(13, 12)
+        Me.btnLoadData.Name = "btnLoadData"
+        Me.btnLoadData.Size = New System.Drawing.Size(105, 34)
+        Me.btnLoadData.TabIndex = 0
+        Me.btnLoadData.Text = "Load Data"
+        Me.btnLoadData.UseVisualStyleBackColor = True
+        '
+        'gbDataMahasiswa
+        '
+        Me.gbDataMahasiswa.Controls.Add(Me.PictureBox1)
+        Me.gbDataMahasiswa.Controls.Add(Me.tbIPKinGB)
+        Me.gbDataMahasiswa.Controls.Add(Me.tbNamaInGB)
+        Me.gbDataMahasiswa.Controls.Add(Me.Label4)
+        Me.gbDataMahasiswa.Controls.Add(Me.tbNimInGB)
+        Me.gbDataMahasiswa.Controls.Add(Me.Label3)
+        Me.gbDataMahasiswa.Controls.Add(Me.Label2)
+        Me.gbDataMahasiswa.Location = New System.Drawing.Point(13, 62)
+        Me.gbDataMahasiswa.Name = "gbDataMahasiswa"
+        Me.gbDataMahasiswa.Size = New System.Drawing.Size(444, 331)
+        Me.gbDataMahasiswa.TabIndex = 1
+        Me.gbDataMahasiswa.TabStop = False
+        Me.gbDataMahasiswa.Text = "Data Mahasiswa"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(276, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(134, 186)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'tbIPKinGB
+        '
+        Me.tbIPKinGB.Location = New System.Drawing.Point(132, 127)
+        Me.tbIPKinGB.Name = "tbIPKinGB"
+        Me.tbIPKinGB.Size = New System.Drawing.Size(100, 22)
+        Me.tbIPKinGB.TabIndex = 7
+        '
+        'tbNamaInGB
+        '
+        Me.tbNamaInGB.Location = New System.Drawing.Point(132, 77)
+        Me.tbNamaInGB.Name = "tbNamaInGB"
+        Me.tbNamaInGB.Size = New System.Drawing.Size(100, 22)
+        Me.tbNamaInGB.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(30, 133)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Label4"
+        '
+        'tbNimInGB
+        '
+        Me.tbNimInGB.Location = New System.Drawing.Point(132, 37)
+        Me.tbNimInGB.Name = "tbNimInGB"
+        Me.tbNimInGB.Size = New System.Drawing.Size(100, 22)
+        Me.tbNimInGB.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "NIM"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 361)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(662, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(481, 73)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(75, 23)
+        Me.btnNext.TabIndex = 3
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(481, 102)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrevious.TabIndex = 4
+        Me.btnPrevious.Text = "Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(481, 131)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Last"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'btnFirst
+        '
+        Me.btnFirst.Location = New System.Drawing.Point(481, 160)
+        Me.btnFirst.Name = "btnFirst"
+        Me.btnFirst.Size = New System.Drawing.Size(75, 23)
+        Me.btnFirst.TabIndex = 6
+        Me.btnFirst.Text = "First"
+        Me.btnFirst.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(481, 215)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.btnEdit.TabIndex = 7
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(481, 244)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 8
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(482, 276)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 16)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "NIM :"
+        '
+        'tbNIM
+        '
+        Me.tbNIM.Location = New System.Drawing.Point(526, 273)
+        Me.tbNIM.Name = "tbNIM"
+        Me.tbNIM.Size = New System.Drawing.Size(100, 22)
+        Me.tbNIM.TabIndex = 10
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(526, 301)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 11
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(339, 29)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 16)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Label5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(662, 383)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.tbNIM)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnFirst)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.gbDataMahasiswa)
+        Me.Controls.Add(Me.btnLoadData)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.gbDataMahasiswa.ResumeLayout(False)
+        Me.gbDataMahasiswa.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLoadData As Button
+    Friend WithEvents gbDataMahasiswa As GroupBox
+    Friend WithEvents tbIPKinGB As TextBox
+    Friend WithEvents tbNamaInGB As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents tbNimInGB As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnPrevious As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents btnFirst As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbNIM As TextBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Data Mahasiswa/Data Mahasiswa/Form1.vb
@@ -0,0 +1,45 @@
+﻿
+Imports System.IO.File
+
+Public Class Form1
+    Dim dataList As New ArrayList()
+
+    Private Sub btnLoadData_Click(sender As Object, e As EventArgs) Handles btnLoadData.Click
+        Dim filePath As String = "D:\Latihan\Desktop\Data Mahasiswa\data.txt"
+        LoadDataFromFile2(filePath)
+        ' Looping untuk mengakses setiap elemen dalam array
+        For i As Integer = 0 To dataList.Count - 1
+            ' Menambahkan elemen ke dalam label dengan tanda koma sebagai pemisah
+            Label5.Text &= dataList(i) & ", "
+        Next
+
+        ' Menghilangkan tanda koma terakhir
+        Label5.Text = Label5.Text.TrimEnd(", ")
+        ' Membuka dialog untuk memilih file gambar
+        Dim imagePath As String = "D:\Latihan\Desktop\Data Mahasiswa\1.png"
+
+        ' Menampilkan gambar di PictureBox
+        PictureBox1.Image = Image.FromFile(imagePath)
+
+        dataList.Clear()
+    End Sub
+
+
+    ' Menampilkan gambar di PictureBox
+
+    Private Sub LoadDataFromFile2(ByVal filePath As String)
+        ' Mendapatkan data dari file teks
+        Dim lines() As String = System.IO.File.ReadAllLines(filePath)
+
+        ' Mendeklarasikan ArrayList untuk menyimpan data
+
+
+        ' Looping untuk memasukkan setiap data ke dalam ArrayList
+        For Each line As String In lines
+            Dim data() As String = line.Split(";")
+            For Each d As String In data
+                dataList.Add(d)
+            Next
+        Next
+    End Sub
+End Class
